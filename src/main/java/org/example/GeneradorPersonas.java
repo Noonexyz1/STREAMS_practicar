02d7ced341df3fe25ed7873444ed73cb0e593f57
Noonexyz1 <aries-d2github@hotmail.com>
2023-04-26 13:23:32 -0400

    mi practica

--- src/main/java/org/example/GeneradorPersonas.java
+++ src/main/java/org/example/GeneradorPersonas.java
@@ -5,8 +5,24 @@ import java.util.List;
 import java.util.Random;
 
 public class GeneradorPersonas {
-    private static final String[] nombres = {"Juan", "Ana", "Pedro", "Maria", "Luis", "Carmen", "Carlos", "Laura"};
-    private static final String[] ciudades = {"Buenos Aires", "Madrid", "Lima", "Santiago", "México", "Bogotá", "Londres", "Nueva York"};
+    private static final String[] nombres = {"María", "Juan", "Ana", "Luis", "Lucía", "Pablo", "Elena", "Javier", "Cristina", "Miguel",
+            "Lucas", "Laura", "Pedro", "Sara", "Carlos", "Ángela", "Fernando", "Adriana", "Sergio", "Beatriz",
+            "David", "Silvia", "Ismael", "Marina", "Diego", "Esther", "Rafael", "Alba", "Antonio", "Paula",
+            "Raúl", "Alicia", "Francisco", "Nuria", "Gabriel", "Carmen", "Manuel", "Inés", "Jorge", "Sandra",
+            "Mario", "Marta", "Óscar", "Celia", "Daniel", "Clara", "Diego", "Teresa", "José", "Victoria",
+            "Iván", "Verónica", "Jesús", "Cristina", "Lorenzo", "Mónica", "Eduardo", "Gloria", "Roberto", "Rocío",
+            "Enrique", "Ainhoa", "Héctor", "Lidia", "Julia", "Mariano", "María", "Joaquín", "Carmen", "Salvador",
+            "Elvira", "Francisco", "Paula", "Guillermo", "Sonia", "Santiago", "Estefanía", "Eva", "Alberto", "Natalia",
+            "José Luis", "Amparo", "Andrés", "Nerea", "Rubén", "Aurora", "Elena", "Jordi", "Paloma", "Xavier"};
+    private static final String[] ciudades = {"Tokio", "Nueva York", "Mumbai", "São Paulo", "Delhi", "Shanghái", "Ciudad de México", "El Cairo", "Dhaka", "Pekín",
+            "Buenos Aires", "Moscú", "Estambul", "Chongqing", "Londres", "Kolkata", "Teherán", "Lima", "Bangkok", "Shenzhen",
+            "Lagos", "Estambul", "Bogotá", "Johannesburgo", "Wuhan", "Taipéi", "Chennai", "Bangalore", "Hong Kong", "Bagdad",
+            "Hanoi", "Riad", "Singapur", "Santiago de Chile", "Madrid", "Toronto", "Dallas", "San Francisco", "Filadelfia", "Miami",
+            "Seattle", "Minneapolis", "Denver", "Houston", "Detroit", "Phoenix", "San Diego", "Tampa", "Orlando", "Pittsburgh",
+            "Washington D.C.", "San Luis", "Portland", "Nashville", "Albuquerque", "Cleveland", "Cincinnati", "Nueva Orleans", "Honolulu",
+            "Austin", "Las Vegas", "Indianápolis", "Charlotte", "Salt Lake City", "Kansas City", "Baltimore", "Milwaukee", "Raleigh", "Omaha",
+            "Memphis", "Louisville", "Jacksonville", "Richmond", "Oklahoma City", "Providence", "Bridgeport", "Charleston", "Wichita", "Buffalo",
+            "Springfield", "Birmingham", "Baton Rouge", "Tucson", "Fargo", "Sioux Falls", "Helena", "Juneau", "Anchorage", "Honolulu"};
 
     public static List<Persona> generarPersonas(int cantidad) {
         List<Persona> personas = new ArrayList<>();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +14,7 @@ public class Main {
         List<String> nombresMayores30 = personas.stream()
                 .filter(p -> p.getEdad() > 30)
                 .map(Persona::getNombre)
-                .collect(Collectors.toList());
+                .toList();
 
         // Imprimir la lista de nombres de personas mayores de 30 años
         System.out.println("Nombres de personas mayores de 30 años: " + nombresMayores30);
@@ -26,5 +27,31 @@ public class Main {
 
         // Imprimir la edad promedio de las personas en la lista
         System.out.println("Edad promedio de las personas en la lista: " + edadPromedio);
+
+
+
+        System.out.println("-------------------------------------------------------");
+        List<Persona> listaDePersonas = GeneradorPersonas.generarPersonas(50);
+        listaDePersonas.forEach(persona -> System.out.println(persona.getNombre()));
+
+        //Imprimir todos nombres de personas sque empiezan con J
+        System.out.println("-------------------------------------------------------");
+        listaDePersonas.stream()
+                .filter(persona -> persona.getNombre().startsWith("J"))
+                .forEach(x -> System.out.println(x.getNombre()));
+
+        //del anterior sacar la edad media de todos los nombre que empiezen con D
+        System.out.println("-------------------------------------------------------");
+        double mediaEdad = listaDePersonas.stream()
+                .filter(x -> x.getNombre().startsWith("J"))
+                .mapToInt(Persona::getEdad)
+                .average()
+                .orElse(0.0);
+
+        System.out.println(mediaEdad);
+
+
     }
+
+
 }
