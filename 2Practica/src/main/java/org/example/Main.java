a095da99ac72a1adcff2e185bbb12b96d07b3837
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-10 10:12:03 -0400

    2Practica: operaciones Map Reduce ejemplo terminado

--- 2Practica/src/main/java/org/example/Main.java
+++ 2Practica/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List jedis = List.of("Yoda", "Windu", "Anakin", "Luke", "Rey");
+
+        // forEach() es una funcion high order
+        jedis.forEach(param -> System.out.println(param));
+        jedis.forEach(System.out::println);
+
+
+        // ejemplo Map, Map. Reduce
+        var theJedis = jedis.stream()
+                .peek(System.out::println)  //peek() para auditoria
+                .filter(s -> !s.equals("Rey"))
+                .peek(System.out::println)
+                .map(j -> j.toString().toUpperCase())
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        System.out.println("Resultado: " + theJedis);
+        
     }
 }
