d91f2244f7dfa8c08c31be1b998dedc1fabe6a13
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-12 14:50:09 -0400

    3Practica: ejemplo parallelStream y stream terminado

--- 3Practica/src/main/java/org/example/Main.java
+++ 3Practica/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -15,7 +15,62 @@ public class Main {
         return numbers;
     }
 
+    public static void sortList(List<Integer> unsortedList){
+        Collections.sort(unsortedList, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+    }
+
+    public static void sortLambdaList(List<Integer> unsortedList){
+        Collections.sort(unsortedList, (o1, o2) -> o2.compareTo(o1));
+    }
+
+    public static boolean isPrimeNumber(int n){
+        if (n <= 1)
+            return false;
+
+        var contador = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                contador++;
+
+        }
+        return contador <= 2;
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println(createRamdomList(5_000_000));
+
+        System.out.println("Iniciando Calculo");
+        //var listadoPrimos = createRamdomList(Integer.parseInt(args[0])).stream()
+        //var listadoPrimos = createRamdomList(50_000_000).parallelStream()
+        var listadoPrimos = createRamdomList(50_000_000).stream()
+                .filter(Main::isPrimeNumber)
+                .sorted()
+                .toList();
+
+        System.out.println("Calculo Finalizado");
+        System.out.println(listadoPrimos);
+
+
+        //Generar un flujo de enteros de 0 a 100
+        /*var primos = IntStream.range(0, 100).boxed()
+                .filter(n -> isPrimeNumber(n))
+                .toList();
+
+        System.out.println(primos);*/
+
+        //Ordenamiento con clase anonima y con funcion lambda
+        /*var randomNumbers = createRamdomList(10);
+        System.out.println(randomNumbers);
+        //sortList(randomNumbers);
+        sortLambdaList(randomNumbers);
+        System.out.println(randomNumbers);*/
+
     }
 }
