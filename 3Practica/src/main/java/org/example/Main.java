fcfa7601cf929031b796a482127686a9cfdb7102
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-12 12:10:54 -0400

    3Practica: createRamdomList(int size) agregado

--- 3Practica/src/main/java/org/example/Main.java
+++ 3Practica/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+
+    public static List<Integer> createRamdomList(int size){
+        var ramdom = new Random();
+        List<Integer> numbers = new LinkedList<>();
+        for (int i = 0; i < size; i++){
+            numbers.add(ramdom.nextInt(100));
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(createRamdomList(5_000_000));
     }
 }
