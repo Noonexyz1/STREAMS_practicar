baf172b7a160ca1e71f0faed8e41156f6197cfb4
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-16 17:26:06 -0400

    4Practica: map, reduce y peek forEach agregados

--- 4Practica/src/main/java/org/example/Main.java
+++ 4Practica/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -19,6 +20,31 @@ public class Main {
                 .filter(emp -> emp.getPuesto().equals("Ingeniero Civil"))
                 .filter(emp -> emp.getEstatura() > 175)
                 .forEach(System.out::println);
+
+        //map
+        empleados.stream()
+                .map(emp -> new Empleado(emp.getId() + 100,
+                                        emp.getNombre(),
+                                        emp.getApellidos(),
+                                        emp.getEdad(),
+                                        emp.getPuesto(),
+                                        emp.getSalario(),
+                                        emp.getEstatura())
+                )
+                .forEach(System.out::println);
+
+        //reduce
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
+        int a = empleados.stream()
+                .map(Empleado::getSalario)
+                .reduce(0, binaryOperator);
+        System.out.println(a);
+
+        //peek().forEach() - modifica la estructura origen (tener mucho cuidado)
+        empleados.stream()
+                .peek(emp -> emp.setId(emp.getId() + 100))
+                .forEach(System.out::println);
+        empleados.forEach(System.out::println);
     }
 
     public static List<Empleado> leerListaArchivoJSON() throws URISyntaxException, IOException {
