e0dda17d10345bde115eab0d8919670ac659573a
Noonexyz1 <aries-d2github@hotmail.com>
2024-04-16 18:58:30 -0400

    4Practica: metodos stream() basicos agregados

--- 4Practica/src/main/java/org/example/Main.java
+++ 4Practica/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BinaryOperator;
 
 public class Main {
@@ -14,14 +15,22 @@ public class Main {
         List<Empleado> empleados = leerListaArchivoJSON();
         empleados.forEach(System.out::println);
 
-        //filter
+        //stream().filter()
         empleados.stream()
                 .filter(emp -> emp.getNombre().startsWith("J"))
                 .filter(emp -> emp.getPuesto().equals("Ingeniero Civil"))
                 .filter(emp -> emp.getEstatura() > 175)
                 .forEach(System.out::println);
 
-        //map
+        //stream().limit()
+        empleados.stream().limit(10)
+                .forEach(System.out::println);
+
+        //stream().skip() - salta los primero n elementos
+        empleados.stream().skip(10)
+                .forEach(System.out::println);
+
+        //stream().map()
         empleados.stream()
                 .map(emp -> new Empleado(emp.getId() + 100,
                                         emp.getNombre(),
@@ -33,17 +42,42 @@ public class Main {
                 )
                 .forEach(System.out::println);
 
-        //reduce
+        //stream().reduce()
         BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
         int a = empleados.stream()
                 .map(Empleado::getSalario)
                 .reduce(0, binaryOperator);
         System.out.println(a);
 
-        //peek().forEach() - modifica la estructura origen (tener mucho cuidado)
+        //stream().count()
+        long numElem = empleados.stream().count();
+        System.out.println(numElem);
+
+        //stream().findFirst()
+        Optional<Empleado> miEmp = empleados.stream().findFirst();
+        System.out.println(miEmp.get());
+
+        //stream().max()
+        Optional<Integer> miEmp2 = empleados.stream()
+                .map(Empleado::getSalario)
+                .max(Integer::compare);
+        System.out.println(miEmp2.get());
+
+
+
+        //stream().peek().forEach() - modifica la estructura origen (tener mucho cuidado)
         empleados.stream()
                 .peek(emp -> emp.setId(emp.getId() + 100))
                 .forEach(System.out::println);
+        empleados.forEach(System.out::println);
+
+
+
+        //lista.removeIf() - modifica la estructura origen (tener cuidado)
+        empleados.removeIf(emp -> emp.getEstatura() < 150);
+        empleados.forEach(System.out::println);
+
+        //lista.forEach()
         empleados.forEach(System.out::println);
     }
 
